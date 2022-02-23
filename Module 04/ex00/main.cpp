887aa95161cdd27b1b403f7d99496fe54736a9dd
4-o-4 <yuzhakov.lev@gmail.com>
2022-02-23 21:19:44 +0300

    refactor: Module 04

--- Module 04/ex00/main.cpp
+++ Module 04/ex00/main.cpp
@@ -7,8 +7,8 @@ int main() {
     const Animal *j = new Dog();
     const Animal *i = new Cat();
 
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
+    std::cout << j->getType() << std::endl;
+    std::cout << i->getType() << std::endl;
     j->makeSound();
     i->makeSound();
     meta->makeSound();
